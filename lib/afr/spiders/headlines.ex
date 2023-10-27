73bd067bafb674c8691da654201a2d5f30e13ac0
tomo <thce28@gmail.com>
2023-10-27 12:11:44 +0100

    Successful scraping of stories from fin-services

--- lib/afr/spiders/headlines.ex
+++ lib/afr/spiders/headlines.ex
@@ -14,27 +14,18 @@ defmodule Headlines do
     # Parse response body to document
     {:ok, document} = Floki.parse_document(response.body)
 
-    case response do
-      %HTTPoison.Response{status_code: 200, body: body} ->
-        # Parse the HTML content using a library like Floki or similar
-        doc = Floki.parse_document(body)
+    # CSS class selector for headlines on afr.com/companies/financial-services
+    items =
+      Floki.find(document, "_2slqK")
+      |> Enum.map(fn story ->
+        %{
+          title: Floki.find(story, "h3") |> Floki.text(),
+          summary: Floki.find(story, "p") |> Floki.text()
+        }
+      end)
 
-        # Define a CSS selector to locate the headlines
-        headlines = Floki.find(doc, "your-css-selector-for-headlines")
+    next_requests = []
 
-        # Extract and process the headlines
-        headlines_data =
-          headlines
-          |> Enum.map(&Floki.text/1)
-
-        # You can further process or store the headlines as needed
-        # For now, we'll just print them
-        IO.inspect(headlines_data)
-
-      _ ->
-        IO.puts("Failed to retrieve the webpage")
-
-        # %{items: items, requests: next_requests}
-    end
+    %{items: items, requests: next_requests}
   end
 end
